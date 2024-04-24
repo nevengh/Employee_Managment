ede5fcc2b3a145544af5498e7383d626e55ad239
Neven <nevenghasoun@gmail.com>
2024-04-24 00:23:23 -1100

    Models Created

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Projects;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Projects $projects)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Projects $projects)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Projects $projects)
+    {
+        //
+    }
+}
